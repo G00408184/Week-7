7778b3f567ae8075677ad4d4c7f361081d972445
Jakub Mamala <g00408184@atu.ie>
2022-11-07 09:51:39 +0000

    InputMismatchException

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -1,17 +1,26 @@
 package ie.atu;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
         System.out.println("please enter a number between 0-9 :");
-        Scanner myscan = new Scanner(System.in);
-        int myNum = myscan.nextInt();
+        try {
+            Scanner myscan = new Scanner(System.in);
+            int myNum = myscan.nextInt();
 
-        if (myNum <= 9 && myNum > 0){
-         System.out.println("you entered " + myNum);
-        }else {
-            System.out.println("Not a valid number");
+            if (myNum <= 9 && myNum > 0) {
+                System.out.println("you entered " + myNum);
+            } else {
+                System.out.println("Not a valid number");
+            }
         }
+            catch(InputMismatchException f)
+            {
+                System.out.println("error recieved");
+            }
+        }
+
     }
-}
+
